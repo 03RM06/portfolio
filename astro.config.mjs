f44eb9a3c53f19b04a15d38f29e8f71a84c3cbb7
Rochmar Maray <roch14552@gmail.com>
2026-06-20 08:19:41 +0800

    Set Netlify site URL in astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
-  site: 'https://lorenzmaray.netlify.app', // update with real domain after Netlify connect
+  site: 'https://resplendent-kulfi-0b4c61.netlify.app',
   integrations: [sitemap()],
   output: 'static',
 });
